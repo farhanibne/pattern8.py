1a7db652084ce0db40b1ea5ccbafa3353c6584d4
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2021-09-06 13:06:18 +0600

    Create a.py

---
+++ a.py
@@ -0,0 +1,9 @@
+import cowsay
+# cowsay.daemon("hi demon")
+# cowsay.dragon("hi dragon")
+# cowsay.beavis("hi beavis")
+# cowsay.cheese("hi chesse")
+# cowsay.cow("hi hamba")
+# cowsay.ghostbusters("hi ghost")
+# cowsay.kitty("hi kitty")
+cowsay.meow("mew mew")
